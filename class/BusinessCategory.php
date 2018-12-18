ee50d67eca52551701ada2045131e7ea0b39599c
KNisansala <kavini@sublime.lk>
2018-12-18 16:45:52 +0530

    #2 Business Category

---
+++ class/BusinessCategory.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * Description of BusinessCategory
+ *
+ * @author W j K n ¨
+ */
+class BusinessCategory {
+    
+    public $id;
+    public $name;
+    public $image_name;
+    public $sort;
+
+    public function __construct($id) {
+        if ($id) {
+
+            $query = "SELECT `id`,`name`,`image_name`,`sort` FROM `business_category` WHERE `id`=" . $id;
+
+            $db = new Database();
+
+            $result = mysql_fetch_array($db->readQuery($query));
+
+            $this->id = $result['id'];
+            $this->name = $result['name'];
+            $this->image_name = $result['image_name'];
+            $this->sort = $result['sort'];
+
+            return $this;
+        }
+    }
+
+    public function create() {
+
+        $query = "INSERT INTO `business_category` ("
+                . "`name`,"
+                . "`image_name`,"
+                . "`sort`) "
+                . "VALUES  ("
+                . "'" . $this->name . "',"
+                . "'" . $this->image_name . "',"
+                . "'" . $this->sort . "'"
+                . ")";
+
+        $db = new Database();
+
+        $result = $db->readQuery($query);
+
+        if ($result) {
+            $last_id = mysql_insert_id();
+
+            return $this->__construct($last_id);
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function all() {
+
+        $query = "SELECT * FROM `business_category` ORDER BY `sort` ASC";
+        $db = new Database();
+        $result = $db->readQuery($query);
+        $array_res = array();
+
+        while ($row = mysql_fetch_array($result)) {
+            array_push($array_res, $row);
+        }
+
+        return $array_res;
+    }
+
+    public function update() {
+
+        $query = "UPDATE  `business_category` SET "
+                . "`name` ='" . $this->name . "', "
+                . "`image_name` ='" . $this->image_name . "', "
+                . "`sort` ='" . $this->sort . "' "
+                . "WHERE `id` = '" . $this->id . "'";
+
+        $db = new Database();
+
+        $result = $db->readQuery($query);
+
+        if ($result) {
+            return $this->__construct($this->id);
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function delete() {
+
+        $query = 'DELETE FROM `business_category` WHERE id="' . $this->id . '"';
+
+        $db = new Database();
+
+        return $db->readQuery($query);
+    }
+    
+    public function arrange($key, $img) {
+        $query = "UPDATE `business_category` SET `sort` = '" . $key . "'  WHERE id = '" . $img . "'";
+        $db = new Database();
+        $result = $db->readQuery($query);
+        return $result;
+    }
+}
