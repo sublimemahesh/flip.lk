b0b92ed066f29966ec0ef33bf8a66c4f548c02d9
KNisansala <kavini@sublime.lk>
2019-03-20 13:05:33 +0530

    #125 Design Corrections of Login Page

--- member/forgot-password.php
+++ member/forgot-password.php
@@ -9,7 +9,7 @@ $CATEGORIES = BusinessCategory::all();
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Landing Page</title>
+        <title>Forgot Password || Flip.lk</title>
         <!-- Required meta tags always come first -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -33,129 +33,56 @@ $CATEGORIES = BusinessCategory::all();
         <link rel="stylesheet" type="text/css" href="css/fonts.min.css">
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
         <link href="css/responsive.css" rel="stylesheet" type="text/css"/>
+        <link href="css/login.css" rel="stylesheet" type="text/css"/>
     </head>
     <body class="landing-page">
-        <div class="content-bg-wrap"></div>
-        <!-- Header Standard Landing  -->
-        <div class="header--standard header--standard-landing" id="header--standard">
-            <div class="container">
-                <div class="header--standard-wrap">
-                    <a href="../" class="logo">
-                        <div class="img-wrap">
-                            <img src="img/logo/logo.jpg" alt="flip.lk">
-                        </div>
-                    </a>
-                    <a href="#" class="open-responsive-menu js-open-responsive-menu">
-                        <svg class="olymp-menu-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-menu-icon"></use></svg>
-                    </a>
-                </div>
-            </div>
-        </div>
+        <?php
+        include './header.php';
+        ?>
+        <div class="header-spacer"></div>
+        <div class="container index-container body-content">
 
-        <!-- ... end Header Standard Landing  -->
-        <div class="header-spacer--standard"></div>
+            <div class="col col-xl-12 order-xl-1 col-lg-12 order-lg-1 col-md-12 col-sm-12 col-12">
+                <div class="form">
+                    <div class="tab-content">
+                        <div id="forgot-password">   
+                            <h1>Forgotten Password?</h1>
+                            <div class="top-bott20 m-l-25 m-r-15">
+                                <?php
+                                if (isset($_GET['message'])) {
 
-        <div class="container login-container">
-            <div class="row display-flex">
-                <div class="col col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                    <div class="landing-content">
-                        <h1>Welcome to the Biggest Social Network in the World</h1>
-                        <p>We are the best and biggest social network with 5 billion active users all around the world. Share you
-                            thoughts, write blog posts, show your favourite music via Stopify, earn badges and much more!
-                        </p>
-                        <!--<a href="#" class="btn btn-md btn-border c-white">Register Now!</a>-->
-                    </div>
-                </div>
-
-                <div class="col col-xl-5 col-lg-6 col-md-12 col-sm-12 col-12">
-
-                    <!-- Login-Registration Form  -->
-
-                    <div class="registration-login-form" id="registration-login-form">
-                        <!-- Nav tabs -->
-                        <ul class="nav nav-tabs" role="tablist">
-                            
-                        </ul>
-
-                        <!-- Tab panes -->
-                        <div class="tab-content">
-                            <div class="tab-pane active" id="home" role="tabpanel" data-mh="log-tab">
-                                <div class="title h6">Forgotten Password?</div>
-                                
-                                <form class="content" id="register" action="post-and-get/member.php" method="post">
-                                    <div class="top-bott20 m-l-25 m-r-15">
-                                        <?php
-                                        
-                                        if (isset($_GET['message'])) {
-
-                                            $MESSAGE = New Message($_GET['message']);
-                                            ?>
-                                            <div class="alert alert-<?php echo $MESSAGE->status; ?>" role = "alert">
-                                                <?php echo $MESSAGE->description; ?>
-                                            </div>
-                                            <?php
-                                        }
-
-                                        $vali = new Validator();
-
-                                        $vali->show_message();
-                                        ?>
+                                    $MESSAGE = New Message($_GET['message']);
+                                    ?>
+                                    <div class="alert alert-<?php echo $MESSAGE->status; ?>" role = "alert">
+                                        <?php echo $MESSAGE->description; ?>
                                     </div>
-                                    <div class="row">
-                                        <div class="col col-12 col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                            <div class="form-group label-floating is-empty">
-                                                <label class="control-label">Your Email</label>
-                                                <input class="form-control" placeholder="" type="email" name="email">
-                                            </div>
-                                            
-                                            <input type="submit" class="btn btn-lg btn-primary full-width" name="check-email"  value="Send Email">
-                                        </div>
-                                    </div>
-                                </form>
+                                    <?php
+                                }
+
+                                $vali = new Validator();
+
+                                $vali->show_message();
+                                ?>
                             </div>
+                            <form class="content" id="register" action="post-and-get/member.php" method="post">
+                                <div class="field-wrap">
+                                    <label>
+                                        Email Address<span class="req">*</span>
+                                    </label>
+                                    <input type="email" required autocomplete="off"  name="email"/>
+                                </div>
+                                <button type="submit" class="button button-block" name="check-email"/>Send Email</button>
+                            </form>
                         </div>
-                    </div>
-                    <!-- ... end Login-Registration Form  -->
-                </div>
+                    </div><!-- tab-content -->
+                </div> <!-- /form -->
             </div>
         </div>
         <!-- JS Scripts -->
         <script src="js/jquery-3.2.1.js"></script>
-        <script src="js/jquery.appear.js"></script>
-        <script src="js/jquery.mousewheel.js"></script>
-        <script src="js/perfect-scrollbar.js"></script>
-        <script src="js/jquery.matchHeight.js"></script>
-        <script src="js/svgxuse.js"></script>
-        <script src="js/imagesloaded.pkgd.js"></script>
-        <script src="js/Headroom.js"></script>
-        <script src="js/velocity.js"></script>
-        <script src="js/ScrollMagic.js"></script>
-        <script src="js/jquery.waypoints.js"></script>
-        <script src="js/jquery.countTo.js"></script>
-        <script src="js/popper.min.js"></script>
-        <script src="js/material.min.js"></script>
-        <script src="js/bootstrap-select.js"></script>
-        <script src="js/smooth-scroll.js"></script>
-        <script src="js/selectize.js"></script>
-        <script src="js/swiper.jquery.js"></script>
-        <script src="js/moment.js"></script>
-        <script src="js/daterangepicker.js"></script>
-        <script src="js/simplecalendar.js"></script>
-        <script src="js/fullcalendar.js"></script>
-        <script src="js/isotope.pkgd.js"></script>
-        <script src="js/ajax-pagination.js"></script>
-        <script src="js/Chart.js"></script>
-        <script src="js/chartjs-plugin-deferred.js"></script>
-        <script src="js/circle-progress.js"></script>
-        <script src="js/loader.js"></script>
-        <script src="js/run-chart.js"></script>
-        <script src="js/jquery.magnific-popup.js"></script>
-        <script src="js/jquery.gifplayer.js"></script>
-        <script src="js/mediaelement-and-player.js"></script>
-        <script src="js/mediaelement-playlist-plugin.min.js"></script>
-        <script src="js/base-init.js"></script>
         <script defer src="fonts/fontawesome-all.js"></script>
         <script src="Bootstrap/dist/js/bootstrap.bundle.js"></script>
+        <script src="js/js/login.js" type="text/javascript"></script>
         <!--custom js-->
 
     </body>
--- member/reset-password.php
+++ member/reset-password.php
@@ -9,7 +9,7 @@ $CATEGORIES = BusinessCategory::all();
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Landing Page</title>
+        <title>Reset Password || Flip.lk</title>
         <!-- Required meta tags always come first -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -33,134 +33,68 @@ $CATEGORIES = BusinessCategory::all();
         <link rel="stylesheet" type="text/css" href="css/fonts.min.css">
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
         <link href="css/responsive.css" rel="stylesheet" type="text/css"/>
+        <link href="css/login.css" rel="stylesheet" type="text/css"/>
     </head>
     <body class="landing-page">
-        <div class="content-bg-wrap"></div>
-        <!-- Header Standard Landing  -->
-        <div class="header--standard header--standard-landing" id="header--standard">
-            <div class="container">
-                <div class="header--standard-wrap">
-                    <a href="../" class="logo">
-                        <div class="img-wrap">
-                            <img src="img/logo/logo.jpg" alt="flip.lk">
-                        </div>
+        <?php
+        include './header.php';
+        ?>
+        <div class="header-spacer"></div>
+        <div class="container index-container body-content">
 
-                    </a>
-                    <a href="#" class="open-responsive-menu js-open-responsive-menu">
-                        <svg class="olymp-menu-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-menu-icon"></use></svg>
-                    </a>
-                </div>
-            </div>
-        </div>
-
-        <!-- ... end Header Standard Landing  -->
-        <div class="header-spacer--standard"></div>
-
-        <div class="container login-container">
-            <div class="row display-flex">
-                <div class="col col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                    <div class="landing-content">
-                        <h1>Welcome to the Biggest Social Network in the World</h1>
-                        <p>We are the best and biggest social network with 5 billion active users all around the world. Share you
-                            thoughts, write blog posts, show your favourite music via Stopify, earn badges and much more!
-                        </p>
-                        <!--<a href="#" class="btn btn-md btn-border c-white">Register Now!</a>-->
-                    </div>
-                </div>
-
-                <div class="col col-xl-5 col-lg-6 col-md-12 col-sm-12 col-12">
-
-                    <!-- Login-Registration Form  -->
-
-                    <div class="registration-login-form" id="registration-login-form">
-                        <!-- Nav tabs -->
-                        <ul class="nav nav-tabs" role="tablist">
-
-                        </ul>
-
-                        <!-- Tab panes -->
-                        <div class="tab-content">
-                            <div class="tab-pane active" id="home" role="tabpanel" data-mh="log-tab">
-                                <div class="title h6">Reset Password?</div>
+            <div class="col col-xl-12 order-xl-1 col-lg-12 order-lg-1 col-md-12 col-sm-12 col-12">
+                <div class="form">
+                    <div class="tab-content">
+                        <div id="forgot-password">   
+                            <h1>Reset Password</h1>
+                            <div class="top-bott20 m-l-25 m-r-15">
                                 <?php
                                 if (isset($_GET['message'])) {
-                                    $MESSAGE = new Message($_GET['message']);
+
+                                    $MESSAGE = New Message($_GET['message']);
                                     ?>
-                                    <div class="alert-position">
-                                        <div class="alert alert-<?php echo $MESSAGE->status; ?>" role = "alert">
-                                            <span id="message"><?php echo $MESSAGE->description; ?></span>
-                                        </div>
+                                    <div class="alert alert-<?php echo $MESSAGE->status; ?>" role = "alert">
+                                        <?php echo $MESSAGE->description; ?>
                                     </div>
                                     <?php
                                 }
+
+                                $vali = new Validator();
+
+                                $vali->show_message();
                                 ?>
-
-                                <form class="content" id="register" action="post-and-get/member.php" method="post">
-                                    <div class="row">
-                                        <p>Please check your email for your code. Your code is 5 characters in length.</p>
-                                        <div class="col col-12 col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                            <div class="form-group label-floating is-empty">
-                                                <label class="control-label">Password Reset Code</label>
-                                                <input class="form-control" placeholder="" type="text" name="code" autocomplete="off" />
-                                            </div>
-                                            <div class="form-group label-floating is-empty">
-                                                <label class="control-label">Your Password</label>
-                                                <input class="form-control" placeholder="" type="password" name="password" autocomplete="off" />
-                                            </div>
-                                            <div class="form-group label-floating is-empty">
-                                                <label class="control-label">Confirm Password</label>
-                                                <input class="form-control" placeholder="" type="password" name="cpassword" autocomplete="off" />
-                                            </div>
-
-                                            <input type="submit" class="btn btn-lg btn-primary full-width" name="reset-password"  value="Send Email">
-                                        </div>
-                                    </div>
-                                </form>
                             </div>
+                            <p>Please check your email for your code. Your code is 5 characters in length.</p>
+                            <form class="content" id="register" action="post-and-get/member.php" method="post">
+                                <div class="field-wrap">
+                                    <label>
+                                        Password Reset Code<span class="req">*</span>
+                                    </label>
+                                    <input type="text" required autocomplete="off"  name="code"/>
+                                </div>
+                                <div class="field-wrap">
+                                    <label>
+                                        Password<span class="req">*</span>
+                                    </label>
+                                    <input type="password" required autocomplete="off"  name="password"/>
+                                </div>
+                                <div class="field-wrap">
+                                    <label>
+                                        Confirm Password<span class="req">*</span>
+                                    </label>
+                                    <input type="password" required autocomplete="off"  name="cpassword"/>
+                                </div>
+                                <button type="submit" class="button button-block" name="reset-password"/>Reset Password</button>
+                            </form>
                         </div>
-                    </div>
-                    <!-- ... end Login-Registration Form  -->
-                </div>
+                    </div><!-- tab-content -->
+                </div> <!-- /form -->
             </div>
-        </div>
-        <!-- JS Scripts -->
-        <script src="js/jquery-3.2.1.js"></script>
-        <script src="js/jquery.appear.js"></script>
-        <script src="js/jquery.mousewheel.js"></script>
-        <script src="js/perfect-scrollbar.js"></script>
-        <script src="js/jquery.matchHeight.js"></script>
-        <script src="js/svgxuse.js"></script>
-        <script src="js/imagesloaded.pkgd.js"></script>
-        <script src="js/Headroom.js"></script>
-        <script src="js/velocity.js"></script>
-        <script src="js/ScrollMagic.js"></script>
-        <script src="js/jquery.waypoints.js"></script>
-        <script src="js/jquery.countTo.js"></script>
-        <script src="js/popper.min.js"></script>
-        <script src="js/material.min.js"></script>
-        <script src="js/bootstrap-select.js"></script>
-        <script src="js/smooth-scroll.js"></script>
-        <script src="js/selectize.js"></script>
-        <script src="js/swiper.jquery.js"></script>
-        <script src="js/moment.js"></script>
-        <script src="js/daterangepicker.js"></script>
-        <script src="js/simplecalendar.js"></script>
-        <script src="js/fullcalendar.js"></script>
-        <script src="js/isotope.pkgd.js"></script>
-        <script src="js/ajax-pagination.js"></script>
-        <script src="js/Chart.js"></script>
-        <script src="js/chartjs-plugin-deferred.js"></script>
-        <script src="js/circle-progress.js"></script>
-        <script src="js/loader.js"></script>
-        <script src="js/run-chart.js"></script>
-        <script src="js/jquery.magnific-popup.js"></script>
-        <script src="js/jquery.gifplayer.js"></script>
-        <script src="js/mediaelement-and-player.js"></script>
-        <script src="js/mediaelement-playlist-plugin.min.js"></script>
-        <script src="js/base-init.js"></script>
-        <script defer src="fonts/fontawesome-all.js"></script>
-        <script src="Bootstrap/dist/js/bootstrap.bundle.js"></script>
-        <!--custom js-->
-
+            <!-- JS Scripts -->
+            <script src="js/jquery-3.2.1.js"></script>
+            <script defer src="fonts/fontawesome-all.js"></script>
+            <script src="Bootstrap/dist/js/bootstrap.bundle.js"></script>
+            <script src="js/js/login.js" type="text/javascript"></script>
+            <!--custom js-->
     </body>
 </html>
